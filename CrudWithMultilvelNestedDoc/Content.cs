995690fd54a27b54d7fca446766069ee541def70
khairul <khairultaher@gmail.com>
2020-02-14 13:41:12 +0600

    multilevel nestest doc add update

---
+++ CrudWithMultilvelNestedDoc/Content.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrudWithMultilvelNestedDoc
+{
+    public class Channel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Episode[] Episodes { get; set; }
+    }
+
+    public class Episode
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Track[] Tracks { get; set; }
+    }
+
+    public class Track
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Like[] Likes { get; set; }
+    }
+
+    public class Like
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CrudWithMultilvelNestedDoc/Program.cs
@@ -0,0 +1,83 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace CrudWithMultilvelNestedDoc
+{
+    public class Program
+    {
+        private static Like NewLike()
+        {
+            return new Like
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = "Like 3"
+            };
+        }
+
+        private static Track NewTrack()
+        {
+            return new Track
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = "Trak 3",
+                Likes = new Like[] { }
+            };
+        }
+
+        public static Episode NewShow()
+        {
+            return new Episode
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = "Episode 3",
+                Tracks = new Track[] { }
+            };
+        }
+
+        public static Channel NewChannel()
+        {
+            return new Channel
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = "Channel 2",
+                Episodes = new Episode[] { }
+            };
+        }
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase("ContentDB");
+            IMongoCollection<Channel> collection = db.GetCollection<Channel>("Channels");
+
+            //Channel chann = NewChannel();
+            //collection.InsertOne(chann);
+            //Episode episode = NewShow();          
+            Track track = NewTrack();
+            Like like = NewLike();
+
+
+            var chanelId = "5e4606e6ae7b090688671416";
+            var episodeId = "5e46071d385a672b0cea0f86";
+            var trackId = "5e460dbe2bc5e70c9cfeac21";
+
+            var filter = Builders<Channel>.Filter.Eq(x => x.Id, chanelId);
+            var arrayFilters = new List<ArrayFilterDefinition>();
+            ArrayFilterDefinition<BsonDocument> episodesFilter = new BsonDocument("e._id", new BsonDocument("$eq", episodeId));
+            ArrayFilterDefinition<BsonDocument> tracksFilter = new BsonDocument("t._id", new BsonDocument("$eq", trackId));
+            arrayFilters.Add(episodesFilter);
+            arrayFilters.Add(tracksFilter);
+
+            var update = Builders<Channel>.Update.Push("Episodes.$[e].Tracks.$[t].Likes", like);
+            var updateOptions = new UpdateOptions { ArrayFilters = arrayFilters };
+
+            var result = collection.UpdateOne(filter, update, updateOptions);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
